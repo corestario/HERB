d67d65a2b6e16f989a9c6fd38a12feeac0a9336d
SK0M0R0KH_PC\SK0M0R0KH <arkonit-t@yandex.ru>
2019-01-09 04:12:10 +0200

    point struct added

--- main.go
+++ main.go
@@ -7,86 +7,94 @@ import (
 	"math/big"
 )
 
-type message struct {
-	Ax *big.Int
-	Ay *big.Int
-	Bx *big.Int
-	By *big.Int
+//Ciphertext is usual ElGamal ciphertext C = (A, B)
+//Here A, B - the elliptic curve's points.
+type Ciphertext struct {
+	A Point
+	B Point
 }
 
-func randbigInt(Ep *elliptic.CurveParams) *big.Int {
-	// creating big random value of type big int
-	max := Ep.P
+//Point represent points on the elliptic curve P = (x, y)
+//Open messages are elliptic curve's points.
+type Point struct {
+	x *big.Int
+	y *big.Int
+}
+
+// creating big random value of type big int
+func randBigInt(E elliptic.Curve) *big.Int {
+	max := E.Params().P
 	y, err := rand.Int(rand.Reader, max)
 	if err != nil {
 	}
 	return y
 }
 
-//message M
-func generateMessage(Ep *elliptic.CurveParams) (*big.Int, *big.Int) {
-	y := randbigInt(Ep)
-	ByteY := y.Bytes()
-	Mx, My := Ep.ScalarMult(Ep.Gx, Ep.Gy, ByteY)
-	return Mx, My
+//Generate random message (point) on the elliptic curve E
+func generateMessage(Ep *elliptic.CurveParams) Point {
+	y := randBigInt(Ep)
+	byteY := y.Bytes()
+	Mx, My := Ep.ScalarMult(Ep.Gx, Ep.Gy, byteY)
+	return Point{Mx, My}
 }
 
-//Encrypt message
-func Encrypt(Ep *elliptic.CurveParams, Mx *big.Int, My *big.Int, Qx *big.Int, Qy *big.Int) message {
+//encrypt message
+func encrypt(Ep *elliptic.CurveParams, M Point, Q Point) Ciphertext {
 	var r *big.Int
-	r = randbigInt(Ep)
-	Byter := r.Bytes()
-	var Em message
-	Em.Ax, Em.Ay = Ep.ScalarMult(Ep.Gx, Ep.Gy, Byter)
-	tempx, tempy := Ep.ScalarMult(Qx, Qy, Byter)
-	Em.Bx, Em.By = Ep.Add(Mx, My, tempx, tempy)
-	return Em
+	r = randBigInt(Ep)
+	byteR := r.Bytes()
+	var Ax, Ay = Ep.ScalarMult(Ep.Gx, Ep.Gy, byteR)
+	var Bx, By = Ep.ScalarMult(Q.x, Q.y, byteR)
+	Bx, By = Ep.Add(M.x, M.y, Bx, By)
+	var C = Ciphertext{Point{Ax, Ay}, Point{Bx, By}}
+	return C
 }
 
-func aggregateMessage(Ep *elliptic.CurveParams, C [1]message) message {
-	var GenC message
-	GenC.Ax, GenC.Ay = C[0].Ax, C[0].Ay
-	GenC.Bx, GenC.By = C[0].Bx, C[0].By
+func aggregateMessage(Ep *elliptic.CurveParams, C [1]Ciphertext) Ciphertext {
+	var genC Ciphertext
+	genC.A.x, genC.A.y = C[0].A.x, C[0].A.y
+	genC.B.x, genC.B.y = C[0].B.x, C[0].B.y
 	//for i := 1; i < 10; i++ {
 	//	GenC.Ax, GenC.Ay = Ep.Add(GenC.Ax, GenC.Ay, C[i].Ax, C[i].Ay)
 	//	GenC.Bx, GenC.By = Ep.Add(GenC.Bx, GenC.By, C[i].Bx, C[i].By)
 	//}
-	return GenC
+	return genC
 }
 
-func decrypt(Ep *elliptic.CurveParams, C message, x *big.Int) (*big.Int, *big.Int) {
+func decrypt(Ep *elliptic.CurveParams, C Ciphertext, x *big.Int) Point {
 	var Mx, My, tempx, tempy *big.Int
 	Bytex := x.Bytes()
-	tempx, tempy = Ep.ScalarMult(C.Ax, C.Ay, Bytex)
+	tempx, tempy = Ep.ScalarMult(C.A.x, C.A.y, Bytex)
 	tempy = tempy.Neg(tempy)
-	Mx, My = Ep.Add(C.Bx, C.By, tempx, tempy)
-	return Mx, My
+	Mx, My = Ep.Add(C.B.x, C.B.y, tempx, tempy)
+	var M = Point{Mx, My}
+	return M
 }
 
 func main() {
 	// creating elliptic curve
 	E := elliptic.P256()
 	Ep := E.Params()
-	// base point
-	//fmt.Println(Ep.Gx, Ep.Gy)
-	// Q point
-	x := randbigInt(Ep)
+	//secret key
+	x := randBigInt(Ep)
 	Bytex := x.Bytes()
+	//public key
 	Qx, Qy := Ep.ScalarMult(Ep.Gx, Ep.Gy, Bytex)
+	var Q = Point{Qx, Qy}
 	//1.
-	var Mx, My *big.Int
-	var C [1]message
+	var M Point
+	var C [1]Ciphertext
 	for i := 0; i < 1; i++ {
-		Mx, My = generateMessage(Ep)
-		fmt.Println(Mx, My)
-		C[i] = Encrypt(Ep, Mx, My, Qx, Qy)
+		M = generateMessage(Ep)
+		fmt.Println(M.x, M.y)
+		C[i] = encrypt(Ep, M, Q)
 	}
 	//3. calculate `C = (A, B)`
-	var GenC message
-	GenC = aggregateMessage(Ep, C)
+	var genC Ciphertext
+	genC = aggregateMessage(Ep, C)
 	//5. Decrypt M
-	var newMx, newMy *big.Int
-	newMx, newMy = decrypt(Ep, GenC, x)
-	fmt.Println(newMx, newMy)
+	var newM Point
+	newM = decrypt(Ep, genC, x)
+	fmt.Println(newM.x, newM.y)
 
 }
