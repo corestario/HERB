07908e4d374c45b2a16d10c7ef3f8877cb2e31b4
Alice <agreeru@mail.ru>
2019-03-13 14:35:38 +0200

    bench with  proofs

--- tests/elgamal_bench_test.go
+++ tests/elgamal_bench_test.go
@@ -8,11 +8,12 @@ import (
 	"github.com/dgamingfoundation/Herb/elgamal"
 	"go.dedis.ch/kyber"
 	"go.dedis.ch/kyber/proof"
+	"go.dedis.ch/kyber/proof/dleq"
 )
 
 func BenchmarkElGamal(b *testing.B) {
-	testCasesN := []int{2, 3, 5, 10, 50, 100}
-	testCasesT := []int{1, 2, 3, 4, 35, 50}
+	testCasesN := []int{3, 5, 10}
+	testCasesT := []int{2, 3, 4}
 	for j, tc := range testCasesN {
 		b.Run(fmt.Sprintf("validators set %d", tc), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
@@ -21,9 +22,22 @@ func BenchmarkElGamal(b *testing.B) {
 					b.Errorf("can't init DKG with error %q", er)
 				} else {
 					b.StartTimer()
-					decryptedMessage, newMessages := elGamalBench(parties, curve, testCasesT[j])
+					decryptedMessage, newMessages, errDLE, errDLK, errRK := elGamalBench(parties, curve, testCasesT[j])
+					//decryptedMessage, newMessages, _, _, _ := elGamalBench(parties, curve, testCasesT[j])
 					b.StopTimer()
-
+					for i, err := range errDLK {
+						if err != nil {
+							b.Errorf("DLK %d-th party isn't valid with error %q", i, err)
+						}
+						if errRK[i] != nil {
+							b.Errorf("RK %d-th party isn't valid with error %q", i, errRK[i])
+						}
+					}
+					for _, err := range errDLE {
+						if err != nil {
+							b.Errorf("DLE %d-th party isn't valid with error %q", i, err)
+						}
+					}
 					expectedMessage := curve.Point().Null()
 					for i, _ := range newMessages {
 						expectedMessage = curve.Point().Add(expectedMessage, newMessages[i])
@@ -39,33 +53,48 @@ func BenchmarkElGamal(b *testing.B) {
 	}
 }
 
-func elGamalBench(parties []elgamal.Participant, curve proof.Suite, tr int) (kyber.Point, []kyber.Point) {
+func elGamalBench(parties []elgamal.Participant, curve proof.Suite, tr int) (kyber.Point, []kyber.Point, []error, []error, []error) {
 	n := len(parties)
 
 	//Any system user generates some message, encrypts and publishes it
 	//We use our validators set (parties) just for example
 	publishedCiphertextes := make([]elgamal.Ciphertext, n)
-
+	DLKproofs := make([][]byte, n)
+	RKproofs := make([][]byte, n)
 	newMessages := make([]kyber.Point, n)
-
+	errDLK := make([]error, n)
+	errRK := make([]error, n)
 	publishChan := publishMessages(parties, curve)
 	for publishedMessage := range publishChan {
 		i := publishedMessage.id
-
+		DLKproofs[i] = publishedMessage.DLKproof
+		RKproofs[i] = publishedMessage.RKproof
 		newMessages[i] = publishedMessage.msg
 		publishedCiphertextes[i] = publishedMessage.published
 	}
 
+	//verify all ciphertexts by parties[1]
+	for i := 0; i < n; i++ {
+		errDLK[i], errRK[i] = parties[1].VerifyCiphertext(curve, DLKproofs[i], publishedCiphertextes[i], RKproofs[i])
+	}
 	//aggregate all ciphertextes
 	commonCiphertext := elgamal.AggregateCiphertext(curve, publishedCiphertextes)
 
 	//decrypt the random
 	decryptParts := make([]kyber.Point, tr)
+	DLEproofs := make([]*dleq.Proof, tr)
+	verKeys := make([]kyber.Point, tr)
 	decrypted := decryptMessages(parties, curve, commonCiphertext, tr)
+	errDLE := make([]error, tr)
 	for msg := range decrypted {
 		i := msg.id
 		decryptParts[i] = msg.msg
+		DLEproofs[i] = msg.DLEproof
+		verKeys[i] = msg.H
 	}
-
-	return elgamal.Decrypt(curve, commonCiphertext, decryptParts, n), newMessages
+	//verify decrypted parts
+	for i := 0; i < tr; i++ {
+		errDLE[i] = parties[1].VerifyDecParts(curve, DLEproofs[i], commonCiphertext, decryptParts[i], verKeys[i])
+	}
+	return elgamal.Decrypt(curve, commonCiphertext, decryptParts, n), newMessages, errDLE, errDLK, errRK
 }
