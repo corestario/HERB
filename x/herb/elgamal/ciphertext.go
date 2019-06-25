83291b77a762402b716b063c4b89ad11896cceb4
Alice <agreeru@mail.ru>
2019-06-25 07:34:34 -0700

    functions  serialize deserialize for ciphertext parts

--- x/herb/elgamal/ciphertext.go
+++ x/herb/elgamal/ciphertext.go
@@ -1,15 +1,21 @@
 package elgamal
 
 import (
+	"bytes"
+	"encoding/base64"
+	"encoding/gob"
+	"fmt"
+
 	"go.dedis.ch/kyber"
+	"go.dedis.ch/kyber/pairing/bn256"
 	"go.dedis.ch/kyber/share"
 )
 
 //Ciphertext is usual ElGamal ciphertext C = (a, b)
 //Here a, b - the elliptic curve's points
 type Ciphertext struct {
-	PointA kyber.Point
-	PointB kyber.Point
+	PointA kyber.Point `json:"pointA"`
+	PointB kyber.Point `json:"pointB"`
 }
 
 //IdentityCiphertext creates ciphertext which is neutral with respect to plaintext group operation (after ciphertext aggregation operation)
@@ -47,4 +53,52 @@ func AggregateCiphertext(group kyber.Group, parts []Ciphertext) Ciphertext {
 	}
 
 	return ct
+}
+
+type CiphertextJSON struct {
+	PointA string `json:"pointA"`
+	PointB string `json:"pointB"`
+}
+
+func NewCiphertextJSON(ciphertext *Ciphertext) (*CiphertextJSON, error) {
+	aBuf := bytes.NewBuffer(nil)
+	aEnc := gob.NewEncoder(aBuf)
+	if err := aEnc.Encode(ciphertext.PointA); err != nil {
+		return nil, fmt.Errorf("failed to encode Point A: %v", err)
+	}
+
+	bBuf := bytes.NewBuffer(nil)
+	bEnc := gob.NewEncoder(bBuf)
+	if err := bEnc.Encode(ciphertext.PointB); err != nil {
+		return nil, fmt.Errorf("failed to encode Point B: %v", err)
+	}
+	return &CiphertextJSON{
+		PointA: base64.StdEncoding.EncodeToString(aBuf.Bytes()),
+		PointB: base64.StdEncoding.EncodeToString(bBuf.Bytes()),
+	}, nil
+}
+
+func (ctJSON *CiphertextJSON) Deserialize() (*Ciphertext, error) {
+	ABytes, err := base64.StdEncoding.DecodeString(ctJSON.PointA)
+	if err != nil {
+		return nil, fmt.Errorf("failed to base64-decode point A: %v", err)
+	}
+	ADec := gob.NewDecoder(bytes.NewBuffer(ABytes))
+	pointA := bn256.NewSuite().G2().Point()
+	if err := ADec.Decode(&pointA); err != nil {
+		return nil, fmt.Errorf("failed to decode point A : %v", err)
+	}
+	BBytes, err := base64.StdEncoding.DecodeString(ctJSON.PointB)
+	if err != nil {
+		return nil, fmt.Errorf("failed to base64-decode point B: %v", err)
+	}
+	BDec := gob.NewDecoder(bytes.NewBuffer(BBytes))
+	pointB := bn256.NewSuite().G2().Point()
+	if err := BDec.Decode(&pointB); err != nil {
+		return nil, fmt.Errorf("failed to decode point B : %v", err)
+	}
+	return &Ciphertext{
+		PointA: pointA,
+		PointB: pointB,
+	}, nil
 }
--- x/herb/types/types.go
+++ x/herb/types/types.go
@@ -1,7 +1,13 @@
 package types
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/dgamingfoundation/HERB/x/herb/elgamal"
+
+	"encoding/base64"
+	"encoding/gob"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -10,4 +16,49 @@ import (
 type CiphertextPart struct {
 	Ciphertext      elgamal.Ciphertext
 	EntropyProvider sdk.AccAddress
+}
+
+type CiphertextPartJSON struct {
+	Ciphertext      string         `json:"ciphertext"`
+	EntropyProvider sdk.AccAddress `json:"entropyprovider"`
+}
+
+func NewCiphertextPartJSON(ciphertextPart *CiphertextPart) (*CiphertextPartJSON, sdk.Error) {
+	ctBuf := bytes.NewBuffer(nil)
+	ctEnc := gob.NewEncoder(ctBuf)
+	ctJSON, _ := elgamal.NewCiphertextJSON(&ciphertextPart.Ciphertext)
+	if err := ctEnc.Encode(ctJSON); err != nil {
+		return nil, sdk.ErrUnknownRequest(fmt.Sprintf("failed to encode ciphertext: %v", err))
+	}
+	return &CiphertextPartJSON{
+		Ciphertext:      base64.StdEncoding.EncodeToString(ctBuf.Bytes()),
+		EntropyProvider: ciphertextPart.EntropyProvider,
+	}, nil
+}
+
+func (ctJSON *CiphertextPartJSON) PartDeserialize() (*CiphertextPart, sdk.Error) {
+	ciphertextJSONBytes, err := base64.StdEncoding.DecodeString(ctJSON.Ciphertext)
+	if err != nil {
+		return nil, sdk.ErrUnknownRequest(fmt.Sprintf("failed to base64-decode ciphertextJSON: %v", err))
+	}
+	ciphertextJSONDec := gob.NewDecoder(bytes.NewBuffer(ciphertextJSONBytes))
+	ciphertextjson := elgamal.CiphertextJSON{"", ""}
+	if err := ciphertextJSONDec.Decode(&ciphertextjson); err != nil {
+		return nil, sdk.ErrUnknownRequest(fmt.Sprintf("failed to decode ciphertext json : %v", err))
+	}
+	ciphertext, err := ciphertextjson.Deserialize()
+	if err != nil {
+		return nil, sdk.ErrUnknownRequest(fmt.Sprintf("failed to decode ciphertext: %v", err))
+	}
+	return &CiphertextPart{
+		Ciphertext:      *ciphertext,
+		EntropyProvider: ctJSON.EntropyProvider,
+	}, nil
+}
+func CiphertextArrayDeserialize([]byte) ([]CiphertextPart, sdk.Error) {
+	return nil, nil
+}
+
+func CiphertextArraySerialize([]CiphertextPart) ([]byte, sdk.Error) {
+	return nil, nil
 }
