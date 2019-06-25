a26acb150692cf691cc00bf8a84bb71cf9c14673
SK0M0R0KH_PC\SK0M0R0KH <arkonit-t@yandex.ru>
2019-06-25 15:59:16 +0200

    add GetAllCiphertext, GetAggregatedCiphertext functions

--- x/herb/keeper.go
+++ x/herb/keeper.go
@@ -2,9 +2,10 @@ package herb
 
 import (
 	"encoding/binary"
+	"go.dedis.ch/kyber/v3"
 
 	"github.com/cosmos/cosmos-sdk/codec"
-	//"github.com/dgamingfoundation/HERB/x/herb/elgamal"
+	"github.com/dgamingfoundation/HERB/x/herb/elgamal"
 	"github.com/dgamingfoundation/HERB/x/herb/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -13,14 +14,16 @@ import (
 // Keeper maintains the link to data storage and exposes methods for the HERB protocol actions
 type Keeper struct {
 	storeKey sdk.StoreKey
+	group kyber.Group
 
 	cdc *codec.Codec
 }
 
 // NewKeeper creates new instances of the HERB Keeper
-func NewKeeper(storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
+func NewKeeper(storeKey sdk.StoreKey, group kyber.Group, cdc *codec.Codec) Keeper {
 	return Keeper{
 		storeKey: storeKey,
+		group: group,
 		cdc:      cdc,
 	}
 }
@@ -32,11 +35,11 @@ func (k Keeper) SetCiphertext(ctx sdk.Context, round uint64, ct types.Ciphertext
 	}
 
 	store := ctx.KVStore(k.storeKey)
-	bz := make([]byte, 8)
-	binary.LittleEndian.PutUint64(bz, round)
+	key := make([]byte, 8)
+	binary.LittleEndian.PutUint64(key, round)
 	var ctList []types.CiphertextPart
-	if store.Has(bz) {
-		ctListBytes := store.Get(bz)
+	if store.Has(key) {
+		ctListBytes := store.Get(key)
 		ctList, err := types.CiphertextArrayDeserialize(ctListBytes)
 		if err != nil {
 			return err
@@ -45,9 +48,37 @@ func (k Keeper) SetCiphertext(ctx sdk.Context, round uint64, ct types.Ciphertext
 	} else {
 		ctList = []types.CiphertextPart{ct}
 	}
-	if newCtListBytes, err := types.CiphertextArraySerialize(ctList); err != nil {
+	newCtListBytes, err := types.CiphertextArraySerialize(ctList)
+	if err != nil {
 		return err
 	}
-	store.Set(bz, newCtListBytes)
+	store.Set(key, newCtListBytes)
 	return nil
+}
+
+// GetAllCiphertext returns all ciphertext parts for the given round
+func (k Keeper) GetAllCiphertext(ctx sdk.Context, round uint64) ([]types.CiphertextPart, sdk.Error) {
+	store := ctx.KVStore(k.storeKey)
+	key := make([]byte, 8)
+	binary.LittleEndian.PutUint64(key, round)
+	if !store.Has(key) {
+		return nil, sdk.ErrUnknownRequest("Unknown round")
+	}
+	ctListBytes := store.Get(key)
+	ctList, err := types.CiphertextArrayDeserialize(ctListBytes)
+	if err != nil {
+		return nil, err
+	}
+	return ctList, nil
+}
+
+// GetAggregatedCiphertext aggregate all sended ciphertext parts in one ciphertext and returns it
+func (k Keeper) GetAggregatedCiphertext(ctx sdk.Context, round uint64) (*elgamal.Ciphertext, sdk.Error) {
+	allParts, err := k.GetAllCiphertext(ctx, round)
+	if err != nil {
+		return nil, err
+	}
+	ctArray := make([]elgamal.Ciphertext, len(allParts))
+	aggregatedCiphertext := elgamal.AggregateCiphertext(k.group, ctArray)
+	return &aggregatedCiphertext, nil
 }
