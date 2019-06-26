7232d95aad188ccfb456f7c486b96fc230a86a36
SK0M0R0KH_PC\SK0M0R0KH <arkonit-t@yandex.ru>
2019-06-26 23:52:21 +0200

    query params

--- x/herb/types/querier.go
+++ x/herb/types/querier.go
@@ -1,1 +1,19 @@
 package types
+
+const (
+	QueryAggregatedCt = "aggregatedCiphertext"
+)
+
+type QueryAggregatedCtParams struct {
+	Round uint64
+}
+
+// NewQueryAggregatedCtParams creates a new instance of QueryAggregatedCtParams
+func NewQueryAggregatedCtParams(round uint64) QueryAggregatedCtParams {
+	return QueryAggregatedCtParams{
+		Round: round,
+	}
+}
+
+
+
