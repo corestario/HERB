27488b0ffe0cd41d954b8d88f8395120065bad4c
SK0M0R0KH_PC\SK0M0R0KH <arkonit-t@yandex.ru>
2019-06-26 14:50:00 +0200

    ciphertext to codec

--- x/herb/types/codec.go
+++ x/herb/types/codec.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/dgamingfoundation/HERB/x/herb/elgamal"
 )
 
 var ModuleCdc = codec.New()
@@ -13,4 +14,6 @@ func init() {
 // RegisterCodec registers concrete types on the Amino codec
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(MsgSetCiphertextPart{}, "herb/MsgSetCiphertextPart", nil)
+
+	cdc.RegisterConcrete(elgamal.Ciphertext{}, "elgamal/ciphertext", nil)
 }
